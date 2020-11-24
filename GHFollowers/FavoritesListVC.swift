59606c562c5290bc57daeaabf7a07e9dadc9f05e
Joo Hee Kim <imjhk03@gmail.com>
2020-11-25 00:54:33 +0900

    App Navigation Setup - UITabBarController & UINavigationController

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Joo Hee Kim on 20. 11. 25..
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
-//  Created by Joo Hee Kim on 20. 11. 24..
+//  Created by Joo Hee Kim on 20. 11. 25..
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
     }
 
-
 }
-
