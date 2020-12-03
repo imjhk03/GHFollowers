a39815990a1fbc689adb2af4543bab4023a7f68d
Joo Hee Kim <imjhk03@gmail.com>
2020-12-03 13:06:46 +0900

    Child View Controller - UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Joo Hee Kim on 20. 12. 03..
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
