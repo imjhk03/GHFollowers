b470dbd7f04ecc2c00f78bb08ad796920417e8a1
Joo Hee Kim <imjhk03@gmail.com>
2020-12-13 22:25:12 +0900

    GFAlertVC, Convenience Init, DateDecoding, Networking Refactor

---
+++ GHFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by Joo Hee Kim on 20. 12. 13..
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        backgroundColor       = .systemBackground
+        layer.cornerRadius    = 16
+        layer.borderWidth     = 2
+        layer.borderColor     = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
